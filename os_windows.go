0f6b31a804af311f54eefed6eb68b7b62811b75b
Alex Saveliev <lyolik@codeminders.com>
2017-01-25 10:40:09 +0200

    Support for OS("")
    - we do this so on Windows we can have a VFS which allows access to /C:/foo, /D:/foo, etc
    - added test case meant to be run on Windows
    - bonus: ability to run Travis CI builds on forked repository
    - bonus: Appveyor integration

---
+++ os_windows.go
@@ -0,0 +1,28 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build windows
+
+package ctxvfs
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var errInvalidPath = errors.New("Cannot resolve absolute path")
+
+func (root osFS) resolve(path string) (string, error) {
+	// Clean the path so that it cannot possibly begin with ../.
+	// If it did, the result of filepath.Join would be outside the
+	// tree rooted at root.  We probably won't ever see a path
+	// with .. in it, but be safe anyway.
+	path = strings.TrimPrefix(filepath.Clean(filepath.Join(string(root), path)), string(os.PathSeparator))
+	if !filepath.IsAbs(path) {
+		return "", errInvalidPath
+	}
+	return path, nil
+}
